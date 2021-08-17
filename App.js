0fb174d82a2cceab9d1615191662f101cfd5e774
frankyfdr <60925424+frankyfdr@users.noreply.github.com>
2021-08-17 11:42:32 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+
+
+import {Notify,sendAlert} from "./notify/Notify"
+import { Button } from 'react-bootstrap'
+
+function App() {
+
+  console.log("@")
+  return (
+    <div className="App">
+      <Button onClick={() => {
+
+        sendAlert('danger', new Date())
+
+      }}>Send</Button>
+
+      <Notify />
+      <div>{new Date().toString()}</div>
+    </div>
+  );
+
+
+}
+
+export default App;
